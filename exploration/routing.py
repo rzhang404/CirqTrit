ff47ad3ed3e93addb460fbf62bc7f556cc67801c
Ray Zhang <ray.zhang@alumni.ubc.ca>
2022-04-01 11:38:36 -0700

    fidelity exploration; placement, routing, and noisy simulation successfully mixed

--- exploration/routing.py
+++ exploration/routing.py
@@ -9,8 +9,8 @@ from cirq.experiments import linear_xeb_fidelity
 
 # Make unconstrained input circuit
 num_input_qubits = 10
-circuit_depth = 10
-op_density = 0.5  # probability at each moment a qubit is to have a gate acting on it
+circuit_depth = 5
+op_density = 0.3  # probability at each moment a qubit is to have a gate acting on it
 gate_domain = {cirq.X: 1,
                cirq.Y: 1,
                cirq.Z: 1,
@@ -30,33 +30,55 @@ in_circ = cirq.testing.random_circuit(qubits=num_input_qubits,
                                       gate_domain=gate_domain)
 
 
-# TODO: make constrained placed/routed circuit
 tk_circ = pytket.extensions.cirq.cirq_to_tk(in_circ)
 # tk_dev = pytket.extensions.cirq.CirqDensityMatrixSimBackend()  # args?
-tk_dev = pytket.routing.SquareGrid()
+tk_dev = pytket.routing.SquareGrid(2,5)
 comp_unit = CompilationUnit(tk_circ, [ConnectivityPredicate(tk_dev)])
-place_and_route = SequencePass([
-    PlacementPass(GraphPlacement(tk_dev)),
-    RoutingPass(tk_dev)])
-place_and_route.apply(comp_unit)
+place = PlacementPass(GraphPlacement(tk_dev))
+place.apply(comp_unit)
 
-out_circ = tk_to_cirq(comp_unit.final_map)
+placed_circ = tk_to_cirq(comp_unit.circuit)
+
+print(placed_circ)
+
+route = RoutingPass(tk_dev)
+route.apply(comp_unit)
+
+out_circ = tk_to_cirq(comp_unit.circuit)
+
+print(out_circ)
 
 reps = 1000
-noise_model = cirq.generalized_amplitude_damp(p=0.1, gamma=0.5)  # TODO: find how to add multiple channels
+noise_model = cirq.generalized_amplitude_damp(p=0.001, gamma=0.5)  # TODO: find how to add multiple channels
 non_noisy_simulator = cirq.DensityMatrixSimulator()
 noisy_simulator = cirq.DensityMatrixSimulator(noise=noise_model)
 
-all_to_all_ideal = non_noisy_simulator.simulate(in_circ)
-all_to_all_distribution = non_noisy_simulator.run(in_circ, repetitions=reps)
+all_to_all_ideal = non_noisy_simulator.simulate(placed_circ)
+# all_to_all_distribution = non_noisy_simulator.run(in_circ, repetitions=reps)
 
-all_to_all_noisy_ideal = noisy_simulator.simulate(in_circ)
-all_to_all_noisy_distribution = noisy_simulator.run(in_circ, repetitions=reps)
+all_to_all_noisy_ideal = noisy_simulator.simulate(placed_circ)
+# all_to_all_noisy_distribution = noisy_simulator.run(in_circ, repetitions=reps)
 
 routed_ideal = non_noisy_simulator.simulate(out_circ)
-routed_distribution = non_noisy_simulator.run(out_circ, repetitions=reps)
+# routed_distribution = non_noisy_simulator.run(out_circ, repetitions=reps)
 
 routed_noisy = noisy_simulator.simulate(out_circ)
-routed_noisy_distribution = noisy_simulator.run(out_circ, repetitions=reps)
+# routed_noisy_distribution = noisy_simulator.run(out_circ, repetitions=reps)
 
-# linear_xeb_fidelity(circuit=routed_noisy)
+
+print(cirq.qis.fidelity(all_to_all_ideal.final_density_matrix,
+                        all_to_all_ideal.final_density_matrix,
+                        (len(all_to_all_ideal.final_density_matrix),)))
+
+print(cirq.qis.fidelity(all_to_all_ideal.final_density_matrix,
+                        all_to_all_noisy_ideal.final_density_matrix,
+                        (len(all_to_all_ideal.final_density_matrix),)))
+
+
+print(cirq.qis.fidelity(all_to_all_ideal.final_density_matrix,
+                        routed_ideal.final_density_matrix,
+                        (len(all_to_all_ideal.final_density_matrix),)))
+
+print(cirq.qis.fidelity(all_to_all_ideal.final_density_matrix,
+                        routed_noisy.final_density_matrix,
+                        (len(all_to_all_ideal.final_density_matrix),)))
--- exploration/small_cirqs.py
+++ exploration/small_cirqs.py
@@ -3,11 +3,12 @@ import cirq
 line_qubits = cirq.LineQubit.range(3)
 
 abstract_circuit = cirq.Circuit(
-    cirq.X(line_qubits[0])**0.5,
-    cirq.CX(line_qubits[0], line_qubits[1]),
-    cirq.CX(line_qubits[1], line_qubits[2]),
-    cirq.CX(line_qubits[0], line_qubits[2]), # long-range control?
-    cirq.measure(line_qubits[2], key='m')
+    # cirq.X(line_qubits[0])**0.5,
+    # cirq.CX(line_qubits[0], line_qubits[1]),
+    # cirq.CX(line_qubits[1], line_qubits[2]),
+    # cirq.CX(line_qubits[0], line_qubits[2]), # long-range control?
+    # cirq.measure(line_qubits[2], key='m')
+    cirq.TOFFOLI(line_qubits[0], line_qubits[1], line_qubits[2])
 )
 
 print("Circuit A:")
@@ -20,7 +21,10 @@ print("Circuit B:")
 print(decomp_circ)
 
 # Simulation syntax
-simulator = cirq.Simulator()
-result = simulator.run(abstract_circuit, repetitions=20)
-print("Results:")
-print(result)
+simulator = cirq.DensityMatrixSimulator()
+abs_result = simulator.simulate(abstract_circuit)
+decomp_result = simulator.simulate(decomp_circ)
+
+
+print(cirq.fidelity(abs_result.final_density_matrix, decomp_result.final_density_matrix, qid_shape=(len(abs_result.final_density_matrix),)))
+print(cirq.fidelity(decomp_result.final_density_matrix, decomp_result.final_density_matrix, qid_shape=(len(abs_result.final_density_matrix),)))
